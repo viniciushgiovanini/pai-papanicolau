9000696d60485489f8eda854cb7442389510cc57
Vinicius Giovanini <vgiovanini@sga.pucminas.br>
2023-10-31 17:57:56 -0300

    interface zoom centralizado

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -1,48 +1,199 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog
+import random
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 #############################################
 #                 Metodos                   #
-#############################################  
-def zoom_in(event):
-    canvas.scale("all", 0, 0, 1.2, 1.2)
+#############################################
+class AutoScrollbar(ttk.Scrollbar):
+    ''' A scrollbar that hides itself if it's not needed.
+        Works only if you use the grid geometry manager '''
 
-def zoom_out(event):
-    canvas.scale("all", 0, 0, 0.8, 0.8)
-    
-    
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            self.grid_remove()
+        else:
+            self.grid()
+            ttk.Scrollbar.set(self, lo, hi)
+
+    def pack(self, **kw):
+        raise tk.TclError('Cannot use pack with this widget')
+
+    def place(self, **kw):
+        raise tk.TclError('Cannot use place with this widget')
+
+
+class Zoom_Advanced(ttk.Frame):
+    ''' Advanced zoom of the image '''
+
+    def __init__(self, mainframe, path):
+
+        ''' Initialize the main Frame '''
+        ttk.Frame.__init__(self, master=mainframe)
+        self.master.title('Zoom with mouse wheel')
+        # Vertical and horizontal scrollbars for canvas
+        vbar = AutoScrollbar(self.master, orient='vertical')
+        hbar = AutoScrollbar(self.master, orient='horizontal')
+        vbar.grid(row=4, column=0, sticky='ns')
+        hbar.grid(row=5, column=0, sticky='we')
+        # Create canvas and put image on it
+        self.canvas = tk.Canvas(self.master, highlightthickness=0,
+                                xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.grid(row=4, column=0, sticky='nswe')
+        self.canvas.update()  # wait till canvas is created
+        vbar.configure(command=self.scroll_y)  # bind scrollbars to the canvas
+        hbar.configure(command=self.scroll_x)
+        # Make the canvas expandable
+        
+        self.master.rowconfigure(4, weight=1)
+        self.master.columnconfigure(0, weight=1)
+
+        # Bind events to the Canvas
+        self.canvas.bind('<Configure>', self.show_image)  # canvas is resized
+        self.canvas.bind('<ButtonPress-1>', self.move_from)
+        self.canvas.bind('<B1-Motion>',     self.move_to)
+        # with Windows and MacOS, but not Linux
+        self.canvas.bind('<MouseWheel>', self.wheel)
+        # only with Linux, wheel scroll down
+        self.canvas.bind('<Button-5>',   self.wheel)
+        # only with Linux, wheel scroll up
+        self.canvas.bind('<Button-4>',   self.wheel)
+        self.image = Image.open(path)  # open image
+        self.image = imagem.resize((453, 442))
+        self.width, self.height = self.image.size
+        self.imscale = 1.0  # scale for the canvaas image
+        self.delta = 1.3  # zoom magnitude
+        # Put image into container rectangle and use it to set proper coordinates to the image
+        self.container = self.canvas.create_rectangle(
+            223, 0, self.width+223, self.height, width=0)
+        # Plot some optional random rectangles for the test purposes
+        minsize, maxsize, number = 5, 20, 10
+        # for n in range(number):
+        #     x0 = random.randint(0, self.width - maxsize)
+        #     y0 = random.randint(0, self.height - maxsize)
+        #     x1 = x0 + random.randint(minsize, maxsize)
+        #     y1 = y0 + random.randint(minsize, maxsize)
+        #     color = ('red', 'orange', 'yellow', 'green', 'blue')[
+        #         random.randint(0, 4)]
+            # self.canvas.create_rectangle(
+            #     x0, y0, x1, y1, fill=color, activefill='black')
+        self.show_image()
+
+    def scroll_y(self, *args, **kwargs):
+        ''' Scroll canvas vertically and redraw the image '''
+        self.canvas.yview(*args, **kwargs)  # scroll vertically
+        self.show_image()  # redraw the image
+
+    def scroll_x(self, *args, **kwargs):
+        ''' Scroll canvas horizontally and redraw the image '''
+        self.canvas.xview(*args, **kwargs)  # scroll horizontally
+        self.show_image()  # redraw the image
+
+    def move_from(self, event):
+        ''' Remember previous coordinates for scrolling with the mouse '''
+        self.canvas.scan_mark(event.x, event.y)
+
+    def move_to(self, event):
+        ''' Drag (move) canvas to the new position '''
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
+        self.show_image()  # redraw the image
+
+    def wheel(self, event):
+        ''' Zoom with mouse wheel '''
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        bbox = self.canvas.bbox(self.container)  # get image area
+        if bbox[0] < x < bbox[2] and bbox[1] < y < bbox[3]:
+            pass  # Ok! Inside the image
+        else:
+            return  # zoom only inside image area
+        scale = 1.0
+        # Respond to Linux (event.num) or Windows (event.delta) wheel event
+        if event.num == 5 or event.delta == -120:  # scroll down
+            i = min(self.width, self.height)
+            if int(i * self.imscale) < 30:
+                return  # image is less than 30 pixels
+            self.imscale /= self.delta
+            scale /= self.delta
+        if event.num == 4 or event.delta == 120:  # scroll up
+            i = min(self.canvas.winfo_width(), self.canvas.winfo_height())
+            if i < self.imscale:
+                return  # 1 pixel is bigger than the visible area
+            self.imscale *= self.delta
+            scale *= self.delta
+        # rescale all canvas objects
+        self.canvas.scale('all', x, y, scale, scale)
+        self.show_image()
+
+    def show_image(self, event=None):
+        ''' Show image on the Canvas '''
+        bbox1 = self.canvas.bbox(self.container)  # get image area
+        # Remove 1 pixel shift at the sides of the bbox1
+        bbox1 = (bbox1[0] + 1, bbox1[1] + 1, bbox1[2] - 1, bbox1[3] - 1)
+        bbox2 = (self.canvas.canvasx(0),  # get visible area of the canvas
+                 self.canvas.canvasy(0),
+                 self.canvas.canvasx(self.canvas.winfo_width()),
+                 self.canvas.canvasy(self.canvas.winfo_height()))
+        bbox = [min(bbox1[0], bbox2[0]), min(bbox1[1], bbox2[1]),  # get scroll region box
+                max(bbox1[2], bbox2[2]), max(bbox1[3], bbox2[3])]
+        if bbox[0] == bbox2[0] and bbox[2] == bbox2[2]:  # whole image in the visible area
+            bbox[0] = bbox1[0]
+            bbox[2] = bbox1[2]
+        if bbox[1] == bbox2[1] and bbox[3] == bbox2[3]:  # whole image in the visible area
+            bbox[1] = bbox1[1]
+            bbox[3] = bbox1[3]
+        self.canvas.configure(scrollregion=bbox)  # set scroll region
+        # get coordinates (x1,y1,x2,y2) of the image tile
+        x1 = max(bbox2[0] - bbox1[0], 0)
+        y1 = max(bbox2[1] - bbox1[1], 0)
+        x2 = min(bbox2[2], bbox1[2]) - bbox1[0]
+        y2 = min(bbox2[3], bbox1[3]) - bbox1[1]
+        if int(x2 - x1) > 0 and int(y2 - y1) > 0:  # show image if it in the visible area
+            # sometimes it is larger on 1 pixel...
+            x = min(int(x2 / self.imscale), self.width)
+            # ...and sometimes not
+            y = min(int(y2 / self.imscale), self.height)
+            image = self.image.crop(
+                (int(x1 / self.imscale), int(y1 / self.imscale), x, y))
+            imagetk = ImageTk.PhotoImage(
+                image.resize((int(x2 - x1), int(y2 - y1))))
+            imageid = self.canvas.create_image(max(bbox2[0], bbox1[0]), max(bbox2[1], bbox1[1]),
+                                               anchor='nw', image=imagetk)
+            self.canvas.lower(imageid)  # set image into background
+            self.canvas.imagetk = imagetk
+
+
 def selecionar_imagem():
     global imagem
     global imagem_tk
-    arquivo = filedialog.askopenfilename(filetypes=[("Imagens", "*.png;*.jpg")])
+    global arquivo
+    arquivo = filedialog.askopenfilename(
+        filetypes=[("Imagens", "*.png;*.jpg")])
     imagem = Image.open(arquivo)
     imagem_tk = ImageTk.PhotoImage(imagem)
-    
-    canvas.configure(state="normal")
-    canvas.create_image(0, 0, anchor="nw", image=imagem_tk)
-    canvas.image = imagem_tk
-   
-    
+
     nome_arquivo = arquivo.split("/")
-    
+
     nome_final_arquivo = nome_arquivo[len(nome_arquivo)-1]
-    
+
     campo_input.insert(0, nome_final_arquivo)
 
-   
-    
+  
+
 #############################################
 #                  MAIN                     #
 #############################################
-
 
 # Inicializar a janela
 windows = tk.Tk()
 windows.geometry("900x600")
 windows.title("Análise do Exame de Papanicolau")
 
-#Configuracao do Grid 
+# Configuracao do Grid
 windows.grid_rowconfigure(0, weight=0)
 windows.grid_columnconfigure(0, weight=1)
 
@@ -50,18 +201,23 @@ windows.grid_columnconfigure(0, weight=1)
 campo_input = tk.Entry(windows, width=60)
 campo_input.grid(row=1, column=0, pady=10)
 
-botao_enviar = tk.Button(windows, text="Selecionar Imagem", command=selecionar_imagem)
+
+botao_enviar = tk.Button(
+    windows, text="Selecionar Imagem", command=selecionar_imagem)
 botao_enviar.grid(row=2, column=0, pady=10)
 
-# Exibir imagem no canvas
-canvas = tk.Canvas(windows, width=400, height=400, state="disabled")
-canvas.grid(row=4,column=0, pady=10)
+botao_preview = tk.Button(
+    windows, text="Abrir Imagem", command=lambda: Zoom_Advanced(windows, path=arquivo))
+botao_preview.grid(row=3, column=0, pady=10)
 
-# Label para exebir a imagem
-label_imagem = tk.Label(windows)
-label_imagem.grid(row=6, column=0, pady=5)
+
+# # Exibir imagem no canvas
+# canvas = tk.Canvas(windows, width=400, height=400, state="disabled")
+# canvas.grid(row=4, column=0, pady=10)
+
+# # Label para exebir a imagem
+# label_imagem = tk.Label(windows)
+# label_imagem.grid(row=6, column=0, pady=5)
 
 # Iniciar o loop da interface
 windows.mainloop()
-
-
